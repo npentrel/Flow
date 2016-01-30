482d69dcb8ee83ad86041804b6839110aefd97df
Miquel Llobet <mllobet.cm@gmail.com>
2016-01-30 17:27:45 +0000

    Added logging

--- backend/reader.py
+++ backend/reader.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 import argparse
 import math
+import time
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
 
-def eeg_handler(unused_addr, args, ch1, ch2, ch3, ch4):
-    print("EEG (uV) per channel: ", ch1, ch2, ch3, ch4)
-
 def concentration_handler(unused_addr, args, ch1):
     print("Concentration: ", ch1)
+    log.write("{0},{1}\n".format(time.time(), ch1))
+    log.flush()
+    #logging.info("{0},{1}".format(time.time(), ch1))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,14 +22,17 @@ if __name__ == "__main__":
                         type=int,
                         default=5000,
                         help="The port to listen on")
+    parser.add_argument("--logfile",
+                        default="muse.log",
+                        help="The Concentration log file")
     args = parser.parse_args()
 
     dispatcher = dispatcher.Dispatcher()
     dispatcher.map("/debug", print)
-    #dispatcher.map("/muse/eeg", eeg_handler, "EEG")
     dispatcher.map("/muse/elements/experimental/concentration", concentration_handler, "Concentration")
     
-    server = osc_server.ThreadingOSCUDPServer(
-        (args.ip, args.port), dispatcher)
-    print("Serving on {}".format(server.server_address))
-    server.serve_forever()
+    with open(args.logfile, 'w') as log:
+        server = osc_server.ThreadingOSCUDPServer(
+            (args.ip, args.port), dispatcher)
+        print("Serving on {}".format(server.server_address))
+        server.serve_forever()
