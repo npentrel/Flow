fbec76d4098a6042bb59146411e71aa2b8fdf883
Feynman Liang <feynman.liang@gmail.com>
2016-01-30 15:35:02 +0000

    Adds basic pdfjs

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -14,7 +14,34 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.hello.onRendered(function() {
+    /* In your Template.xxx.rendered */
+    // Set worker URL to package assets
+    PDFJS.workerSrc = '/packages/pascoual_pdfjs/build/pdf.worker.js';
+    // Create PDF
+    var url = 'http://arxiv.org/pdf/1601.07596.pdf'
+    PDFJS.getDocument(url).then(function getPdfHelloWorld(pdf) {
+      // Fetch the first page
+      pdf.getPage(1).then(function getPageHelloWorld(page) {
+        var scale = 1;
+        var viewport = page.getViewport(scale);
+
+        // Prepare canvas using PDF page dimensions
+        var canvas = document.getElementById('pdfcanvas');
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Render PDF page into canvas context
+        page.render({canvasContext: context, viewport: viewport}).promise.then(function () {
+          console.log('Rendered');
+        });
+      });
+    });
+  })
 }
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
